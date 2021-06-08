e2c2cdc40cb99e4f2d9ad80fbdc1b21efa8fd65b
liangbiao <zhailb666@163.com>
2021-06-08 21:49:39 +0800

    temp

--- lerna-demo/packages/lb-t-one/src/index.js
+++ lerna-demo/packages/lb-t-one/src/index.js
@@ -3,6 +3,6 @@
  * @Date: 2021-06-08 20:43:09
  * @Description: file content
  */
-export const main = (a) => {
+export const main = function (a) {
   console.log('dist-one-src1', JSON.stringify(a))
 };
