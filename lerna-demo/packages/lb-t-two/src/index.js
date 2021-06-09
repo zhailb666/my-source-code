e17f6b6e9685e3930882b4d81f8c8051d9bf87ed
zhailiangbiao <zhailiangbiao@accesscorporate.com.cn>
2021-06-09 10:19:33 +0800

    move package

--- lerna-demo/packages/lb-t-two/src/index.js
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: your name
- * @Date: 2021-06-08 20:43:09
- * @Description: file content
- */
-export const main = function (a) {
-  console.log('dist-src-two1', JSON.stringify(a))
-};
