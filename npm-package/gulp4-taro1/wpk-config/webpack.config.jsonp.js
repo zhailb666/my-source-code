4f129daf37f7329a6944bf99853a2a7ff8b1f739
liangbiao <zhailb666@163.com>
2021-06-16 22:49:56 +0800

    gulp-taro1

---
+++ npm-package/gulp4-taro1/wpk-config/webpack.config.jsonp.js
@@ -0,0 +1,21 @@
+/*
+ * @Author: your name
+ * @Date: 2021-06-02 22:07:20
+ * @Description: file content
+ */
+const { merge }  = require('webpack-merge');
+const common = require('../webpack.common.js');
+const path = require('path');
+
+module.exports = merge(common, {
+  entry: './es/index.js',
+  output: {
+    filename: 'gulp4-and-webpack4-jsonp.js',
+    library: {
+      name: 'MyLibrary',
+      // type: 'umd', // 很多type|| amd, umd, commonjs, module, jsonp
+      type: 'jsonp', // 很多type|| amd, umd, commonjs, module, jsonp
+    },
+    path: path.resolve(__dirname, '../dist'),
+  },
+});
